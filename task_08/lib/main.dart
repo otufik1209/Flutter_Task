768c9cb4c9bce3d9441b6ac70db78caa68fed789
Toufik <toufikmunshi1209@gmail.com>
2025-04-10 18:08:26 +0600

    task_08

---
+++ task_08/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task 8',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      drawer: AppDrawer(),
+      body: Center(child: Text('Home Screen')),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile')),
+      drawer: AppDrawer(),
+      body: Center(child: Text('Profile Screen')),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      drawer: AppDrawer(),
+      body: Center(child: Text('Settings Screen')),
+    );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blueGrey),
+            child: Text('Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24)),
+          ),
+          _createDrawerItem(context, Icons.home_rounded, 'Home', HomeScreen()),
+          _createDrawerItem(context, Icons.person, 'Profile', ProfileScreen()),
+          _createDrawerItem(
+              context, Icons.settings, 'Settings', SettingsScreen()),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _createDrawerItem(
+    BuildContext context, IconData icon, String title, Widget screen) {
+  return ListTile(
+    leading: Icon(icon, color: Colors.blueGrey),
+    title: Text(title),
+    onTap: () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+    },
+  );
+}
