c7c104049e107fb42c38775ef6c68dc29fc4474d
Toufik <toufikmunshi1209@gmail.com>
2025-04-12 00:06:29 +0600

    task_11

---
+++ task_11/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Local Image App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Local Image Example'))),
+      body: Center(child: Image.asset('assets/images/mazda.jpg')),
+    );
+  }
+}
